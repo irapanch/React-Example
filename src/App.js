031f8185e38c9261179a2addb6e58b23194fd418
irapanch <117355580+irapanch@users.noreply.github.com>
2024-01-05 11:52:07 +0200

    example get data in props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Section } from "./components/Section";
 
 function App() {
+  const movies = [
+    { id: 1, title: "Step up" },
+    { id: 2, title: "Taxi" },
+  ];
   return (
     <div className="App">
       <h1>Hello world</h1>
       <Section title="Books" />
-      <Section />
+      <Section data={movies} />
     </div>
   );
 }
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export const Section = ({ title = "Default" }) => {
+export const Section = ({ title = "Default", data = [] }) => {
   return (
     <section>
       <h1>{title}</h1>
       <ul>
-        <li>1</li>
-        <li>2</li>
+        <li>{data[0]?.title}</li>
+        <li>{data[1]?.title}</li>
         <li>3</li>
       </ul>
     </section>
