7bca080ed3fcbb105dfa860f965440e979c3ea89
irapanch <117355580+irapanch@users.noreply.github.com>
2024-01-05 14:23:58 +0200

    ternar && render

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ function App() {
     { id: 4, title: "Route" },
   ];
   const btns = ["get", "push", "patch", "delete"];
+  const isOnline = true;
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>{!isOnline ? "Hello my friend" : "Let`s login"}</h1>
+      <h2>{isOnline && "Online"}</h2>
       <Button titleBtn="Login" />
       <Button titleBtn="Password" />
       <Button titleBtn="add" />
