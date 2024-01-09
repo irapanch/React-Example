ac0c5b471af23c38d8daaf5d46cff70d4c8b9249
irapanch <117355580+irapanch@users.noreply.github.com>
2024-01-09 16:35:52 +0200

    GlobalStyles

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import { GlobalSyles } from "./styles/GlobalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>
+    <GlobalSyles />
+  </>
 );
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle, styled } from "styled-components";
+
+export const GlobalSyles = createGlobalStyle`
+body{
+    background-color: lightgrey;
+    .title{
+        color: darkblue;
+    }
+}
+`;
