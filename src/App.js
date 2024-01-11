bc5d2dfe93d73893893ef190e596d8ceb0073b9a
irapanch <117355580+irapanch@users.noreply.github.com>
2024-01-11 09:13:53 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,65 +1,17 @@
-import { Button } from "./components/Button/Button";
-import { Modal } from "./components/Modal/Modal";
-import { Section } from "./components/Section/Section";
-import { Flex } from "./styles/GlobalStyles";
+import React from "react";
+import { Counter } from "./components/Counter/Counter.jsx";
+import { FlexContainer } from "./components/Counter/Counter.styled";
+// import { ColorPicker } from "./components/ColorPicker/ColorPicker";
+// import { TodoList } from "./components/TodoList/TodoList";
 
-function App() {
-  const movies = [
-    { id: 1, title: "Step up" },
-    { id: 2, title: "Taxi" },
-    { id: 3, title: "Flexi" },
-    { id: 4, title: "Route" },
-  ];
-  // const btns = ["get", "push", "patch", "delete"];
-  const isOnline = false;
+const App = () => {
   return (
-    <div className="App">
-      <h1 className="title">{!isOnline ? "Hello my friend" : "Let`s login"}</h1>
-      <hr></hr>
-      <h2>{isOnline && "Online"}</h2>
-      {!isOnline && <h2>Offline</h2>}
-      <hr></hr>
-      <Flex
-        $direction="column"
-        $justify="center"
-        $align="center"
-        $minHeight="50vh"
-        $gap="20px"
-      >
-        <div>
-          <Button titleBtn="Login" />
-          <Button titleBtn="Password" />
-          <Button titleBtn="add" />
-        </div>
-      </Flex>
-      <Section title="Movie1" data={movies} />
-      <Section title="Movie2" data={movies} />
-
-      <hr></hr>
-      {/* {btns.map((btn) => (
-        <Button key={btn} titleBtn={btn}>
-          {" "}
-          Hello
-        </Button>
-      ))} */}
-      <hr></hr>
-      <Modal>
-        <Button>Hello from Modal</Button>
-        <h1 className="title">React </h1>
-      </Modal>
-      <Modal>
-        em Eligendi minima consequatur laboriosam odit quibusdam, esse magnam
-        nostrum voluptatem, velit est earum nemo omnis animi repudiandae iste
-        facere! Eos autem et vitae nam ducimus doloremque quasi, id adipisci
-        aliquid. Aspernatur expedita ipsa vero cumque, obcaecati soluta quod
-        odio facere dolores impedit architecto eligendi laboriosam qui
-        cupiditate voluptates odit
-      </Modal>
-      <Modal>
-        <img src="https://react.dev/images/og-home.png" alt="react" />
-      </Modal>
-    </div>
+    <>
+      <FlexContainer>
+        <Counter />
+      </FlexContainer>
+    </>
   );
-}
+};
 
 export default App;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,10 +2,39 @@ export const theme = {
   colors: {
     main: "#e2e2e4",
     accent: "teal",
-    bgMain: "lightblue",
+    bgMain: "yellow",
+    fontColor: "black",
+  },
+  backgrounds: {
+    main: "white",
   },
   shadows: {
     default: "2px 1px 2px 1px gray",
+    xl: "4px 2px 3px 3px black",
   },
-  spacing: (value) => `${value * 4}px`,
+  media: {
+    tablet: "568px",
+    desktop: "1024px",
+  },
+  spacing: (value, coef = 4) => `${value * coef}px`,
+};
+export const darkTheme = {
+  colors: {
+    main: "#e2e2e4",
+    fontColor: "white",
+    accent: "teal",
+    bgMain: "yellow",
+  },
+  backgrounds: {
+    main: "black",
+  },
+  shadows: {
+    default: "2px 1px 2px 1px gray",
+    xl: "4px 2px 3px 3px black",
+  },
+  media: {
+    tablet: "568px",
+    desktop: "1024px",
+  },
+  spacing: (value, coef = 4) => `${value * coef}px`,
 };
