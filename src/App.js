180670db18f79db73f4d5ce30718e21b26f9f106
irapanch <117355580+irapanch@users.noreply.github.com>
2024-02-03 20:53:01 +0200

    react-hook-form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from "react";
 // import { FlexContainer } from "./components/Counter/Counter.styled";
 // import RegisterForm from "./components/RegisterForm/RegisterForm";
 import { StyledFlex } from "./styledComponents/StyledFlex";
-import LoginFormik from "./components/Login/LoginFormik";
+// import LoginFormik from "./components/Login/LoginFormik";
+import LoginHookForm from "./components/Login/LoginHookForm";
 // import { Employee } from "./components/Employees/Employee";
 // import Login from "./components/Login/Login";
 // import { ColorPicker } from "./components/ColorPicker/ColorPicker";
@@ -12,7 +13,7 @@ import LoginFormik from "./components/Login/LoginFormik";
 const App = () => {
   return (
     <StyledFlex $center>
-      <LoginFormik />
+      <LoginHookForm />
     </StyledFlex>
   );
 };
