eae4bbeaf151a9e3af03e1013d7f45a01684fc42
irapanch <117355580+irapanch@users.noreply.github.com>
2024-02-04 14:27:07 +0200

    didMount, didUpdate,willUnmount

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-// import { Counter } from "./components/Counter/Counter.jsx";
+import { Counter } from "./components/Counter/Counter.jsx";
 // import { FlexContainer } from "./components/Counter/Counter.styled";
 // import RegisterForm from "./components/RegisterForm/RegisterForm";
 import { StyledFlex } from "./styledComponents/StyledFlex";
+
 // import LoginFormik from "./components/Login/LoginFormik";
-import LoginHookForm from "./components/Login/LoginHookForm";
+// import LoginHookForm from "./components/Login/LoginHookForm";
 // import { Employee } from "./components/Employees/Employee";
 // import Login from "./components/Login/Login";
 // import { ColorPicker } from "./components/ColorPicker/ColorPicker";
@@ -13,7 +14,7 @@ import LoginHookForm from "./components/Login/LoginHookForm";
 const App = () => {
   return (
     <StyledFlex $center>
-      <LoginHookForm />
+      <Counter />
     </StyledFlex>
   );
 };
