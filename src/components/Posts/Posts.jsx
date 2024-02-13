77aaaede667252d9a7ab167b3cbc3dafddc7215c
irapanch <117355580+irapanch@users.noreply.github.com>
2024-02-13 16:02:43 +0200

    get posts

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,7 +1,31 @@
 import React, { Component } from "react";
+import { Header } from "./Header";
+import { PostList } from "./PostList";
+import { fetchPosts } from "../../services/postApi";
 
 export default class Posts extends Component {
+  state = {
+    posts: [],
+    limit: null,
+  };
+  async componentDidMount() {
+    try {
+      const { posts, limit } = await fetchPosts({ limit: 3 }); // деструктуризація постів з data
+      this.setState({ posts, limit });
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
   render() {
-    return <div>Posts</div>;
+    const { posts } = this.state;
+    return (
+      <div>
+        <Header />
+        <section>
+          <PostList posts={posts} />
+        </section>
+      </div>
+    );
   }
 }
