697e4d7502959720f13044db486c0021c89166f9
irapanch <117355580+irapanch@users.noreply.github.com>
2024-02-21 10:21:23 +0200

    colorpiker refact use hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
 // import Posts from "./components/Posts/Posts";
-import { Counter } from "./components/Counter/Counter.jsx";
+// import { Counter } from "./components/Counter/Counter.jsx";
 // import { FlexContainer } from "./components/Counter/Counter.styled";
 // import RegisterForm from "./components/RegisterForm/RegisterForm";
-import { StyledFlex } from "./styledComponents/StyledFlex";
+// import { StyledFlex } from "./styledComponents/StyledFlex";
 
 // import LoginFormik from "./components/Login/LoginFormik";
 // import LoginHookForm from "./components/Login/LoginHookForm";
 // import { Employee } from "./components/Employees/Employee";
 // import Login from "./components/Login/Login";
-// import { ColorPicker } from "./components/ColorPicker/ColorPicker";
+import { ColorPicker } from "./components/ColorPicker/ColorPicker";
 // import { TodoList } from "./components/TodoList/TodoList";
 
 const App = () => {
-  return <StyledFlex $center>
-    <Counter/>
-  </StyledFlex>;
-  // return <Posts />;
+  // return <StyledFlex $center>
+  //   <Counter/>
+  // </StyledFlex>;
+  return <ColorPicker/>;
 };
 
 export default App;
