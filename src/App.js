8b0dba60840b0b171c8012115f571709ec8b15fc
irapanch <117355580+irapanch@users.noreply.github.com>
2024-03-04 11:23:09 +0200

    counter useReduser, initialState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useContext } from "react";
-import {Posts} from "./components/Posts/Posts";
-import { MyContext } from "./context/ContextProvider";
-import Login from "./components/Login/Login";
-// import { Counter } from "./components/Counter/Counter.jsx";
+// import {Posts} from "./components/Posts/Posts";
+// import { MyContext } from "./context/ContextProvider";
+// import Login from "./components/Login/Login";
+import { Counter } from "./components/Counter/Counter.jsx";
 // import Component1 from "./components/Component1.jsx";
 // import { FlexContainer } from "./components/Counter/Counter.styled";
 // import RegisterForm from "./components/RegisterForm/RegisterForm";
-import { StyledFlex } from "./styledComponents/StyledFlex";
+// import { StyledFlex } from "./styledComponents/StyledFlex";
 import { ThemeContext } from "./context/ThemeProviderContext";
 
 // import LoginFormik from "./components/Login/LoginFormik";
@@ -20,34 +20,38 @@ import { ThemeContext } from "./context/ThemeProviderContext";
 
 
 const App = () => {
-  const {user, isLoggedIn, logout} =  useContext(MyContext)
-  if (!isLoggedIn){
-   return <StyledFlex $center>
-    <Login/>
-  </StyledFlex>;
-  }
+  // const {user, isLoggedIn, logout} =  useContext(MyContext)
+  // if (!isLoggedIn){
+  //  return <StyledFlex $center>
+  //   <Login/>
+  // </StyledFlex>;
+  // }
 
   const {theme, changeTheme} = useContext(ThemeContext)
 
-  return (
-  <div style={{background: theme === 'dark' ? 'lightgray' : 'white'}}>
-  <h1>{user.name}</h1>
-  <h2>{user.email}</h2>
-  <button onClick={changeTheme}>SWITCH THEME</button>
-  <button onClick={logout}>LOGOUT</button>
-  < Posts />;
-  </div>
-  )
+  // return (
+  // <div style={{background: theme === 'dark' ? 'lightgray' : 'white'}}>
+  // <h1>{user.name}</h1>
+  // <h2>{user.email}</h2>
+  // <button onClick={changeTheme}>SWITCH THEME</button>
+  // <button onClick={logout}>LOGOUT</button>
+  // < Posts />;
+  // </div>
+  // )
   // return <StyledFlex $center>
   //   <Login/>
   // </StyledFlex>;
 
-  // return (
-  // 
-  // < Counter  />
-  // <Component1 />
-  // 
-  // )
+  return (
+
+    <div style={{background: theme === 'dark' ? 'lightgray' : 'white'}}>
+      <button onClick={changeTheme}>SWITCH THEME</button>
+  < Counter  />
+  </div>
+
+    
+  
+  )
 
 };
 
