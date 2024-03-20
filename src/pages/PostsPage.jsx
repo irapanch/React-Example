27235fb77fa44d90a2c991c8e25cdc343875791a
irapanch <117355580+irapanch@users.noreply.github.com>
2024-03-20 16:18:11 +0200

    /posts/search?q=${query}

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -4,45 +4,49 @@ import { Posts } from '../components/Posts/Posts'
 import { useHttp } from '../hooks/useHttp'
 import { getPostsByQuery } from '../services/postApi'
 import { useForm } from 'react-hook-form'
-import { useSearchParams } from 'react-router-dom'
-
-const PostsPage = () => {
-   
-  return (
-   <Posts/>
-  )
-}
-
-export default PostsPage
+import { Link, useSearchParams } from 'react-router-dom'
 
 // const PostsPage = () => {
-//   const {
-//     register,
-//     handleSubmit
-//   } = useForm()
-  
-//   const submit = (data) => {
    
-//     setSearchParams({test: data.queryStr })
-//   }
-
-//   const[searchParams, setSearchParams] = useSearchParams()
-//     const {data} = useHttp(getPostsByQuery, '')
 //   return (
-//     <>
-//     <form onSubmit={handleSubmit(submit)}>
-//       <input {...register('queryStr', {required: true, minLength:2 })} type='text'/>
-//       <button>Search</button>
-//     </form>
-//     <ol>
-//         {data?.map(post => (
-//             <li key={post.id}>
-//                <h2>{post.title}</h2>
-//             </li>))}
-//     </ol>
-//     </>
-    
+//    <Posts/>
 //   )
 // }
 
 // export default PostsPage
+
+const PostsPage = () => {
+  const {
+    register,
+    handleSubmit
+  } = useForm()
+  
+  const submit = (data) => {
+   
+    setSearchParams(data.queryStr ? {query: data.queryStr } : {})
+  }
+
+  const[searchParams, setSearchParams] = useSearchParams()
+  const query = searchParams.get('query' )  || ''
+  const {data} = useHttp(getPostsByQuery, query )
+    
+    console.log(query);
+  return (
+    <>
+    <form onSubmit={handleSubmit(submit)}>
+      <input {...register('queryStr')} type='text'/>
+      <button>Search</button>
+    </form>
+    <ol>
+        {data?.map(post => (
+            <li key={post.id}>
+              <Link to={post.id.toString()}><h2>{post.title}</h2></Link>
+               
+            </li>))}
+    </ol>
+    </>
+    
+  )
+}
+
+export default PostsPage
