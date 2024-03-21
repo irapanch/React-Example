689858376d8d33ff054685676cd4e28c0c2d4a78
irapanch <117355580+irapanch@users.noreply.github.com>
2024-03-21 12:37:01 +0200

    goBackRef - useRef

--- src/pages/SinglePostPage.jsx
+++ src/pages/SinglePostPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link, Outlet, useLocation, useParams } from 'react-router-dom'
 import { useHttp } from '../hooks/useHttp'
 import { getPostsById } from '../services/postApi'
@@ -7,10 +7,11 @@ const SinglePostPage = () => {
     const {postId} = useParams()
     const {data} = useHttp(getPostsById, postId)
     const location = useLocation()
+    const goBackRef = useRef(location.state?.from || '/')
 
   return (
     <>
-    <Link to={location.state?.from}>Go back</Link>
+    <Link to={goBackRef.current}>Go back</Link>
      <h1>Post #{postId} </h1>
     <hr/>
     <h2>{data?.title}</h2>
